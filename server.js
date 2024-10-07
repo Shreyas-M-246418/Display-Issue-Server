4af385ee7457c086d924f0e1bad3b6cdcdcbf321
Shreyas M <shreyas.m246418@gmail.com>
2024-10-07 10:38:38 +0530

    updated server

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-
+/*
 const express = require('express');
 const axios = require('axios');
 const cors = require('cors');
@@ -19,6 +19,44 @@ app.post('/api/auth/github', async (req, res) => {
     const response = await axios.post('https://github.com/login/oauth/access_token', {
       client_id: process.env.GITHUB_CLIENT_ID,
       client_secret: process.env.GITHUB_CLIENT_SECRET,
+      code,
+    }, {
+      headers: { Accept: 'application/json' },
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    res.status(500).json({ message: 'Error exchanging code for token', error: error.message });
+  }
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on http://localhost:${PORT}`);
+});*/
+
+
+const express = require('express');
+const axios = require('axios');
+const cors = require('cors');
+//const dotenv = require('dotenv');
+
+//dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors({
+  origin: process.env.CLIENT_URL || 'http://localhost:3000'
+}));
+app.use(express.json());
+
+app.post('/api/auth/github', async (req, res) => {
+  const { code } = req.body;
+
+  try {
+    const response = await axios.post('https://github.com/login/oauth/access_token', {
+      client_id:"Ov23li2EE68XAUxEK2Vm",
+      client_secret: "e5a0837ccd64f5a2322b7fddddff225438b82847",
       code,
     }, {
       headers: { Accept: 'application/json' },
